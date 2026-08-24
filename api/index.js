e9909668adacc918c48ecd02548e0ab4a4c561ad
Satish Chandra Karki <aihalkarki@gmail.com>
2026-08-24 21:36:18 +0545

    Fix database connection import bug on main website

--- api/index.js
+++ api/index.js
@@ -1,10 +1,6 @@
 const ejs = require('ejs');
 const path = require('path');
-const { createClient } = require('@vercel/kv');
-const kv = createClient({
-    url: process.env.KV_REST_API_URL || process.env.UPSTASH_REDIS_REST_URL,
-    token: process.env.KV_REST_API_TOKEN || process.env.UPSTASH_REDIS_REST_TOKEN,
-});
+const { kv, isDbConnected } = require('./utils/db');
 
 module.exports = async (req, res) => {
     const default_data = {
